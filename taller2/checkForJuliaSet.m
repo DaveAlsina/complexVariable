63bd4f0adef0d46556ddbd2471214eb09e51fa5b
DaveAlsina <58042964+DaveAlsina@users.noreply.github.com>
2021-02-21 23:00:52 -0500

    corrección crucial en los exponentes de z, para graficar el set de julia

--- taller2/checkForJuliaSet.m
+++ taller2/checkForJuliaSet.m
@@ -1,11 +1,11 @@
-function [color] = checkForJuliaSet(z0, c, maxIterations)
+function [color] = checkForJuliaSet(z0, c, n, maxIterations)
 
 	z = z0;				%%  guarda una copia de la variable compleja que fue creada
 	color = log(1);		%%	pone un color por defecto
 
 	for i = 1:maxIterations	%% análisis de la convergencia
 		
-		z = (z.*z) + c;		%% aplicación de la función de Julia
+		z = (z.^n) + c;		%% aplicación de la función de Julia
 
 		if ( abs(z) > 2 )		%% si el módulo de z excede 2 retorne el color
 			color = log(i);		%% de z, relacionado a la cantidad de iteraciones 
