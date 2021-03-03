91952e420a5a22e9c12fbf8b1fb173c49d3f5a30
DaveAlsina <58042964+DaveAlsina@users.noreply.github.com>
2021-03-02 20:21:10 -0500

    borrado de archivo redundante

--- taller3/createCicloidCoordinates.m
+++
@@ -1,5 +0,0 @@
-function [x, y, complex] = createHipocicloidCoordinates(scalingFactor, radious, t)
-    x = (scalingFactor*radious - radious)*cos(t) + radious*cos(t*(1 - scalingFactor));
-    y = (scalingFactor*radious - radious)*sin(t) + radious*sin(t*(1 - scalingFactor));
-    complex = x + 1i*y;
-end
