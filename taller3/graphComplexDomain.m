9952a50cace4aa4700a2a3e5eadcec3269ffe946
DaveAlsina <58042964+DaveAlsina@users.noreply.github.com>
2021-03-07 22:32:12 -0500

    creación de funcion para graficación de dominio, graficación de dominio de las espirales arquimedianas, modificación de la función de graficación sobre la esfera de riemann para añadir títulos y nombre a los ejes

---
+++ taller3/graphComplexDomain.m
@@ -0,0 +1,10 @@
+function [] = graphComplexDomain(x, y, Title, color)
+
+grid on;
+title(Title)
+xlabel('Real');
+ylabel('Imaginario');
+plot(x,y);
+
+
+end
--- taller3/riemann.m
+++ taller3/riemann.m
@@ -10,42 +10,64 @@ close all
 k = 2.1;        %constante de agrandado del radio mayor respecto al radio menor
 r2 = 1;         %tamaño del radio menor
 
-t = 0:0.01:200*pi; %rango del parámetro para ayudar a graficar
+t = 0:0.01:20*pi; %rango del parámetro para ayudar a graficar
 
 [x,y,z] = createHipocicloidCoordinates(k, r2, t);
 
 %% Espirales arquimedeanas
 center = 2 + 2.*i;
 [x1,y1,z1] = createArchimedeanSpiralCoords(2, 0.0001, t, center);
-[x2,y2,z2] = createArchimedeanSpiralCoords(4, 0.0001, t, center);
-[x3,y3,z3] = createArchimedeanSpiralCoords(8, 0.00001, t, center);
-
+center = 0 + 0.*i;
+[x2,y2,z2] = createArchimedeanSpiralCoords(2, 0.0001, t, center);
 
 %% grafica del hipocicloide
 hold on
-graphProyecionInRiemmanSphere(z, 'Blue')
-pause(10);
-graphProyecionInRiemmanSphere(-z, 'Red')
+    graphProyectionInRiemmanSphere(z, 'Blue', 'Grafica 0 de multiples hipocicloides')
+    graphProyectionInRiemmanSphere(-z, 'Red', 'Grafica 0 de multiples hipocicloides')
 hold off 
+
 pause(10);
 close;
 
-%% grafica de las espirales
+%% grafica 1 de las espirales
 
+%grafica del domino 1 de la grafica 1 de espirales
+hold on;
+    graphComplexDomain(x1, y1, 'Grafica 1 de multiples espirales de Arquimedes', 'Red');
+    graphComplexDomain(-x1, -y1, 'Grafica 1 de multiples espirales de Arquimedes', 'Green');
+    graphComplexDomain(x1, -y1, 'Grafica 1 de multiples espirales de Arquimedes', 'Blue');
+    graphComplexDomain(-x1, y1, 'Grafica 1 de multiples espirales de Arquimedes', 'Black');
+hold off;
+pause(10);
+close;
+
+%proyección 1 de las espirales del dominio 1 (anterioteme mencionado)
 hold on
-graphProyecionInRiemmanSphere(z1, 'Red')
-graphProyecionInRiemmanSphere(-z2, 'Green')
-graphProyecionInRiemmanSphere(z2.*i, 'Blue')
-graphProyecionInRiemmanSphere(-z3.*i, 'Black')
+    graphProyectionInRiemmanSphere(z1, 'Red', 'Proyección 1 de multiples espirales de Arquimedes');
+    graphProyectionInRiemmanSphere(-z1, 'Green','Proyección 1 de multiples espirales de Arquimedes');
+    graphProyectionInRiemmanSphere(z1.*i, 'Blue', 'Proyección 1 de multiples espirales de Arquimedes');
+    graphProyectionInRiemmanSphere(-z1.*i, 'Black', 'Proyección 1 de multiples espirales de Arquimedes');
 hold off
+pause(100);
+close;
+
+%% grafica 2 de las espirales
+
+%grafica del domino 2 de la grafica 2 de espirales
+hold on;
+    graphComplexDomain(x2, y2, 'Grafica 2 de multiples espirales de Arquimedes', 'Red');
+    graphComplexDomain(-x2, -y2, 'Grafica 2 de multiples espirales de Arquimedes', 'Green');
+    graphComplexDomain(x2, -y2, 'Grafica 2 de multiples espirales de Arquimedes', 'Blue');
+    graphComplexDomain(-x2, y2, 'Grafica 2 de multiples espirales de Arquimedes', 'Black');
+hold off;
 pause(10);
 close;
 
-hold on
-graphProyecionInRiemmanSphere(z1 - center, 'Red')
-graphProyecionInRiemmanSphere(-(z2 - center), 'Green')
-graphProyecionInRiemmanSphere((z2 - center).*i, 'Blue')
-graphProyecionInRiemmanSphere(-(z3 - center).*i, 'Black')
 
+hold on
+    graphProyectionInRiemmanSphere(z2, 'Red', 'Proyección 2 de multiples espirales de Arquimedes');
+    graphProyectionInRiemmanSphere(-z2, 'Green', 'Proyección 2 de multiples espirales de Arquimedes');
+    graphProyectionInRiemmanSphere(z2.*i, 'Blue', 'Proyección 2 de multiples espirales de Arquimedes');
+    graphProyectionInRiemmanSphere(-z2.*i, 'Black','Proyección 2 de multiples espirales de Arquimedes' );
 pause(10);
 hold off
