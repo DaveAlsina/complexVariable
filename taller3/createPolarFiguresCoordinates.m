efc7f75d0fa69c5c150fc55e302a22c209519102
Juan Luis <you@example.com>
2021-03-07 23:02:46 -0500

    Se corrigen comentarios

--- taller3/createPolarFiguresCoordinates.m
+++ taller3/createPolarFiguresCoordinates.m
@@ -1,8 +1,8 @@
 function [x,y,complex] = createPolarFiguresCoordinates(sincos,n,r)
 % Esta función crea rosas en coordenadas polares. Recibe como parámetros
-% 'sincos' que es si se realiza la rosa por medio de la función seno o
+% 'sincos' que indica si se realiza la rosa por medio de la función seno o
 % coseno; 'n' que es una rosa de n pétalos si n es impar o 2n pétalos si n
-% es par; 'r' que es la máxima disctancia que toma el radio.
+% es par; 'r' que es la máxima distancia que toma el radio.
 
     theta = 0:0.005:2*pi;
     x = zeros(1,length(theta));
