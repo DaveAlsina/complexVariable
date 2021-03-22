15a48f9317a380729b8d5d768ab7b401ffbaa96d
DaveAlsina <58042964+DaveAlsina@users.noreply.github.com>
2021-03-21 20:08:06 -0500

    pequeños comentarios añadidos

--- taller3/createArchimedeanSpiralCoords.m
+++ taller3/createArchimedeanSpiralCoords.m
@@ -5,12 +5,16 @@ function [x, y, z] = createArchimedeanSpiralCoords(thetaFactor, spacingFactor, t
     % factor de espaciado (b) que va variando en las distintas
     % espirales generadas
 
+    %contador para guardar los datos en los vectores x, y e z
     position = 1;
+    
+    %vectores para guardar los datos que se van generando
     x = zeros(1, length(theta));
     y = zeros(1, length(theta));
     z = zeros(1, length(theta));
     
-   
+   %generación de los puntos de las espirales arquimedeanas siguiendo 
+   %la fórmula de contrucción de estas espirales
     for angle = theta
       r = (spacingFactor) + thetaFactor.*angle;
       y(position) = (r .* sin(thetaFactor .* angle)) + real(zCenter);
--- taller3/riemann.m
+++ taller3/riemann.m
@@ -57,7 +57,7 @@ hold on
     graphProyectionInRiemmanSphere(z1.*i, 'Blue', 'Proyección 1 de multiples espirales de Arquimedes');
     graphProyectionInRiemmanSphere(-z1.*i, 'Black', 'Proyección 1 de multiples espirales de Arquimedes');
 hold off
-pause(60);
+pause(10);
 close;
 
 %% grafica 2 de las espirales
@@ -78,7 +78,7 @@ hold on
     graphProyectionInRiemmanSphere(-z2, 'Green', 'Proyección 2 de multiples espirales de Arquimedes');
     graphProyectionInRiemmanSphere(z2.*i, 'Blue', 'Proyección 2 de multiples espirales de Arquimedes');
     graphProyectionInRiemmanSphere(-z2.*i, 'Black','Proyección 2 de multiples espirales de Arquimedes' );
-pause(60);
+pause(10);
 hold off
 
 
