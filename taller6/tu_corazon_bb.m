46e0e1d6bd3a9501f09059e60e8ec63a2585a1b0
DaveAlsina <58042964+DaveAlsina@users.noreply.github.com>
2021-05-09 19:27:29 -0500

    añadido el análisis para ruido aumentado en tu corazón

--- taller6/tu_corazon_bb.m
+++ taller6/tu_corazon_bb.m
@@ -58,7 +58,63 @@ hold off
 %% 
 visualize_XY_noisy_signal(x, real(z2_reconstruct), y, imag(z2_reconstruct), theta);
 
-% fuentes: 
+
+%% Análisis del nivel del ruido en la imágen
+% para esto se crearán imágenes con más ruido para poder 
+% comparar con el caso anterior que tenía menos ruido
+
+rng(0,'twister');
+noise1 = 2.*randn(length(x),1);
+
+rng(7,'twister');
+noise2 = 2.*randn(length(x),1);
+
+x4 = x + noise1';
+y4 = y + noise2';
+
+z3_noisy = complex(x4, y4);
+
+%% visualización de la señal ruidosa en X(parte real) e Y(parte imaginaria)
+
+visualize_XY_noisy_signal(x, x4, y, y4, theta);
+
+%% visualización de la señal ruidosa completa en el plano 
+% contrastada con su versión no ruidosa
+
+visualize_noisy_signal(z, z3_noisy, ['corazón ruidoso (ruido extra)']);
+
+%% obtención de los coeficientes de fourier y graficado de estos
+% también se hace en estas lineas el filtrado agresivo de la señal
+[z3_fft_clean] = plot_clean_vs_noisy_fft(theta, dt, max(abs(z3_noisy))/20, z3_noisy);
+z3_reconstruct = ifft(z3_fft_clean);
+
+%% obtención de los coeficientes de fourier filtrados 
+%con un filtro menos agresivo
+
+z_fft_3 = fft(z3_noisy);
+z_fft_clean3 = z_fft_3; 
+z_fft_clean3(2:98) = 0;
+z3_reconstruct2 = ifft(z_fft_clean3);
+
+%% graficado de los ángulos de los coeficientes de fourier
+plot_angles_fft(z, z3_noisy);
+
+%% muestra de la señal reconstruida a partir del filtrado de los 
+% coeficientes de fourier y la ifft
+figure 
+hold on
+    plot(z3_reconstruct, '--.b');
+    plot(z3_reconstruct2, '--r');
+    title(['Señal de corazón reconstruida [Ruido Aumentado]']);
+    legend(['Filtro +'],['Filtro -']);
+hold off
+%% 
+visualize_XY_noisy_signal(x, real(z3_reconstruct), y, imag(z3_reconstruct), theta);
+
+
+
+
+%% fuentes: 
 % https://youtu.be/c249W6uc7ho?list=PLMrJAkhIeNNT_Xh3Oy0Y4LTj0Oxo8GqsC
 % https://www.mathworks.com/help/matlab/ref/fft.html
 
